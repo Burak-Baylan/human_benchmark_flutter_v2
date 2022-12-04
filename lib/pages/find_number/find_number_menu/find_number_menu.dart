0e7ddf10fbdcce56d27bdb5318501607d3694b2e
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-04 03:49:38 +0300

    add: find numbers menu

---
+++ lib/pages/find_number/find_number_menu/find_number_menu.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/route_manager.dart';
+import 'package:human_benchmark_flutter_v2/pages/find_number/view/find_number_view.dart';
+import 'package:human_benchmark_flutter_v2/widgets/button/custom_button_with_border.dart';
+
+import '../../../helpers/colors.dart';
+import '../../../widgets/text/less_futured_text.dart';
+
+class FindNumberMenu extends StatelessWidget {
+  const FindNumberMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(''),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: MyColors.secondaryColor,
+            size: 25.w,
+          ),
+          onPressed: () => Get.back(),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(15.sp),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              LessText.lessFuturedText(
+                text: 'Find Number',
+                color: MyColors.secondaryColor,
+                fontSize: 50.sp,
+                fontWeight: FontWeight.w700,
+              ),
+              SizedBox(height: 5.h),
+              LessText.lessFuturedText(
+                text: 'Find the number that is the same as the word.',
+                color: MyColors.secondaryColor,
+                fontSize: 20.sp,
+                fontWeight: FontWeight.w300,
+              ),
+              SizedBox(height: 50.h),
+              CustomButtonWithBorder(
+                onPressed: () {
+                  Get.back();
+                  Get.to(FindNumberView());
+                },
+                text: 'Play',
+                leading: Icon(
+                  Icons.play_arrow_rounded,
+                  size: 25.sp,
+                  color: MyColors.secondaryColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

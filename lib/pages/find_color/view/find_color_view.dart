4b9f23b7add6e4fc397df6b16d1110aee663e74c
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-08 00:22:03 +0300

    add: find color view

---
+++ lib/pages/find_color/view/find_color_view.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import '../../../helpers/colors.dart';
+import '../../../utils/injection_helper.dart';
+import '../../../widgets/text/less_futured_text.dart';
+import '../../sequence_memory/values/const_values.dart';
+import '../view_model/find_color_view_model.dart';
+
+class FindColorView extends StatefulWidget {
+  FindColorView({Key? key}) : super(key: key);
+
+  @override
+  State<FindColorView> createState() => _FindColorViewState();
+}
+
+class _FindColorViewState extends State<FindColorView> {
+  FindColorViewModel findColorVm = getit<FindColorViewModel>();
+  late List<Widget> widgetList = [];
+
+  @override
+  void dispose() {
+    unregisterFindColorViewModel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    findColorVm.play();
+    widgetList = List.generate(9, (index) => _buildCard(index));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    findColorVm.play();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Observer(builder: (_) {
+          return LessText.lessFuturedText(
+            text: '${findColorVm.levelCount}/4',
+            color: MyColors.secondaryColor,
+            fontSize: 25.sp,
+            fontWeight: FontWeight.w300,
+          );
+        }),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: MyColors.secondaryColor,
+            size: 25.w,
+          ),
+          onPressed: () => Get.back(),
+        ),
+      ),
+      body: Observer(builder: (_) {
+        return AnimatedContainer(
+          width: context.width,
+          height: context.height,
+          duration: Consts.findNumberViewBackgroundDuration,
+          color: findColorVm.backgroundColor,
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Observer(builder: (_) {
+                  return Container(
+                    padding: EdgeInsets.symmetric(vertical: 15.w),
+                    margin: EdgeInsets.all(20.w),
+                    width: double.infinity,
+                    color: MyColors.secondaryColor,
+                    child: LessText.lessFuturedText(
+                      text: findColorVm.randomColorText,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 24,
+                    ),
+                  );
+                }),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20.w),
+                  child: GridView.count(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                    children: widgetList,
+                  ),
+                ),
+                SizedBox(height: 50.h),
+              ],
+            ),
+          ),
+        );
+      }),
+    );
+  }
+
+  Widget _buildCard(int index) {
+    return InkWell(
+      onTap: () =>
+          findColorVm.userClicked(index), //findNumberVm.userClicked(index),
+      //onTapDown: (details) => sequenceMemoryVm.cardTapDown(index),
+      //onTapUp: (details) => sequenceMemoryVm.cardTapCancel(index),
+      //onTapCancel: () => sequenceMemoryVm.cardTapCancel(index),
+      child: Observer(builder: (_) {
+        return Container(
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: MyColors.secondaryColor, width: 2),
+            color: findColorVm.colorList[index],
+          ),
+          child: Container(),
+        );
+      }),
+    );
+  }
+}

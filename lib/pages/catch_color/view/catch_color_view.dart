c79f9d5b4bb82d46ed19b17b59d3a7415953d9ea
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-08 19:31:59 +0300

    add: catch color view

---
+++ lib/pages/catch_color/view/catch_color_view.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:human_benchmark_flutter_v2/pages/catch_color/view_model/catch_color_view_model.dart';
+import 'package:human_benchmark_flutter_v2/widgets/app_bar.dart';
+
+import '../../../helpers/colors.dart';
+import '../../../utils/injection_helper.dart';
+
+class CatchColorView extends StatefulWidget {
+  CatchColorView({Key? key}) : super(key: key);
+
+  @override
+  State<CatchColorView> createState() => _CatchColorViewState();
+}
+
+class _CatchColorViewState extends State<CatchColorView> {
+  late List<Widget> widgetList = [];
+
+  CatchColorViewModel catchColorVm = getit<CatchColorViewModel>();
+
+  @override
+  void initState() {
+    widgetList = List.generate(9, (index) => _buildCard(index));
+    catchColorVm.play();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    unregisterCatchColorViewmodel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(builder: (_) {
+      return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight),
+          child: CustomAppBar('${catchColorVm.levelCount}/4'),
+        ),
+        backgroundColor: catchColorVm.backgroundColor,
+        body: Center(
+          child: Container(
+            color: catchColorVm.backgroundColor,
+            margin: EdgeInsets.symmetric(horizontal: 20.w),
+            child: GridView.count(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              crossAxisCount: 3,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+              children: widgetList,
+            ),
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget _buildCard(int index) {
+    return InkWell(
+      onTap: () => catchColorVm.userClicked(index),
+      child: Observer(builder: (_) {
+        return Container(
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: MyColors.secondaryColor, width: 2),
+            color: catchColorVm.selectedIndex == index
+                ? Colors.green
+                : Colors.white,
+          ),
+          child: Container(),
+        );
+      }),
+    );
+  }
+}

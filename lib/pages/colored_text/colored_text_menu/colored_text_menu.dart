15cbb6872869856e69a671757bbdd8fddcc4c2a8
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-16 15:34:58 +0300

    add: colored text menu

---
+++ lib/pages/colored_text/colored_text_menu/colored_text_menu.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/route_manager.dart';
+import 'package:human_benchmark_flutter_v2/pages/colored_text/view/colored_text_view.dart';
+import '../../../helpers/colors.dart';
+import '../../../widgets/button/custom_button_with_border.dart';
+import '../../../widgets/text/less_futured_text.dart';
+
+class ColoredTextMenu extends StatelessWidget {
+  const ColoredTextMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(''),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: MyColors.secondaryColor,
+            size: 25.w,
+          ),
+          onPressed: () => Get.back(),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(15.sp),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              LessText.lessFuturedText(
+                text: 'Colored Text',
+                color: MyColors.secondaryColor,
+                fontSize: 50.sp,
+                fontWeight: FontWeight.w700,
+              ),
+              SizedBox(height: 5.h),
+              LessText.lessFuturedText(
+                text: 'Click on the screen as soon as the text and color are the same.',
+                color: MyColors.secondaryColor,
+                fontSize: 20.sp,
+                fontWeight: FontWeight.w300,
+              ),
+              SizedBox(height: 50.h),
+              CustomButtonWithBorder(
+                onPressed: () {
+                  Get.back();
+                  Get.to(ColoredTextView());
+                },
+                text: 'Play',
+                leading: Icon(
+                  Icons.play_arrow_rounded,
+                  size: 25.sp,
+                  color: MyColors.secondaryColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

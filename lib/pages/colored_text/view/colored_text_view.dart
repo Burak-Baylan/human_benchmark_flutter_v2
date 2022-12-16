a4c470a7f1bc6a61aee8039a6f446032f69f23a2
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-16 15:35:06 +0300

    add: colored text game view model

---
+++ lib/pages/colored_text/view/colored_text_view.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../helpers/colors.dart';
+import '../../../utils/injection_helper.dart';
+import '../../../widgets/app_bar.dart';
+import '../../../widgets/text/less_futured_text.dart';
+import '../view_model/colored_text_view_model.dart';
+
+class ColoredTextView extends StatefulWidget {
+  const ColoredTextView({super.key});
+
+  @override
+  State<ColoredTextView> createState() => _ColoredTextViewState();
+}
+
+class _ColoredTextViewState extends State<ColoredTextView> {
+  ColoredTextViewModel coloredTextVm = getit<ColoredTextViewModel>();
+
+  @override
+  void initState() {
+    coloredTextVm.play();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    unregisterColoredTextViewmodel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: Observer(
+          builder: (context) => CustomAppBar('${coloredTextVm.levelCount}/4'),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(height: 25.h),
+          Observer(builder: (_) {
+            return LessText.lessFuturedText(
+              text: coloredTextVm.changingText,
+              fontWeight: FontWeight.w800,
+              color: coloredTextVm.changingColor,
+              fontSize: 30.sp,
+            );
+          }),
+          SizedBox(height: 25.h),
+          Expanded(
+            child: GestureDetector(
+              onTap: () => coloredTextVm.userClicked(),
+              child: Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(bottom: 20.h, left: 10.w, right: 10.w),
+                decoration: BoxDecoration(
+                  border: Border.all(color: MyColors.secondaryColor, width: 2),
+                ),
+                child: LessText.lessFuturedText(
+                  text: 'Click!',
+                  color: MyColors.secondaryColor,
+                  fontSize: 50.sp,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 25.h),
+        ],
+      ),
+    );
+  }
+}

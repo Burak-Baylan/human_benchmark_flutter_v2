b663c1a7e40eb73ae1a21eab8001d111fb50a2c3
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-16 16:30:21 +0300

    add: math game view, math game view model created

---
+++ lib/pages/math/view/math_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MathView extends StatefulWidget {
+  const MathView({super.key});
+
+  @override
+  State<MathView> createState() => _MathViewState();
+}
+
+class _MathViewState extends State<MathView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/math/view_model/math_view_model.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+part 'math_view_model.g.dart';
+
+class MathViewModel = _MathViewModelBase with _$MathViewModel;
+
+abstract class _MathViewModelBase with Store {
+  
+}

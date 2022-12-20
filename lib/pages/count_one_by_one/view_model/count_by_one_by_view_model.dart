9bb3c1403de57618541646e3198c7ee5273b1242
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-20 03:32:19 +0300

    add: count one by one game view model

---
+++ lib/pages/count_one_by_one/view_model/count_by_one_by_view_model.dart
@@ -0,0 +1,97 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:mobx/mobx.dart';
+
+import '../../../utils/injection_helper.dart';
+import '../../result_page/result_page.dart';
+import '../view/count_one_by_one.dart';
+
+part 'count_by_one_by_view_model.g.dart';
+
+class CountOneByOneViewModel = _CountOneByOneViewModelBase
+    with _$CountOneByOneViewModel;
+
+abstract class _CountOneByOneViewModelBase with Store {
+  List<int> numbersList = [];
+  Random rnd = Random();
+
+  String resultPageTitle = 'Average Time';
+  String get resultPageExp => '$getTotalMs milliseconds';
+  String resultPageMessage = 'Try Again. You can do better.';
+
+  int get getTotalMs => stopWatch.elapsedMilliseconds + extraMs;
+  int extraMs = 0;
+
+  @observable
+  Color backgroundColor = Colors.white;
+
+  Stopwatch stopWatch = Stopwatch();
+
+  void startCounter() => stopWatch.start();
+  void stopCounter() => stopWatch.stop();
+  void resetCounter() => stopWatch.reset();
+
+  @observable
+  int levelCount = 1;
+
+  void play() {
+    fillNumbersList();
+    startCounter();
+  }
+
+  @action
+  void userClick(String number) {
+    if (levelCount.toString() == number) {
+      levelCount++;
+      if (levelCount == 17) {
+        gameDone();
+        return;
+      }
+      return;
+    }
+    wrongAnswerSignal();
+    extraMs += 1000;
+  }
+
+  void gameDone() {
+    Get.back();
+    Get.to(goToResulPage);
+    stopCounter();
+    resetCounter();
+  }
+
+  Widget get goToResulPage => ResultPage(
+        title: resultPageTitle,
+        exp: resultPageExp,
+        message: resultPageMessage,
+        tryAgainPressed: () {
+          Get.to(const CountOneByOneView());
+          registerCountOneByOneViewModel();
+        },
+      );
+
+  void fillNumbersList() {
+    for (var i = 1; i <= 16; i++) {
+      numbersList.add(i);
+    }
+  }
+
+  @action
+  Future<void> wrongAnswerSignal() async {
+    backgroundColor = Colors.redAccent.withOpacity(.4);
+    await Future.delayed(const Duration(milliseconds: 300));
+    backgroundColor = Colors.white;
+  }
+
+  int getNumber() {
+    int rndNum = rnd.nextInt(numbersList.length);
+    int number = numbersList[rndNum];
+    numbersList.removeAt(rndNum);
+    return number;
+  }
+
+  int getRandomNumber([int from = 0, int to = 17]) =>
+      rnd.nextInt(to - from) + from;
+}
---
+++ lib/pages/count_one_by_one/view_model/count_by_one_by_view_model.g.dart
@@ -0,0 +1,74 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'count_by_one_by_view_model.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$CountOneByOneViewModel on _CountOneByOneViewModelBase, Store {
+  late final _$backgroundColorAtom = Atom(
+      name: '_CountOneByOneViewModelBase.backgroundColor', context: context);
+
+  @override
+  Color get backgroundColor {
+    _$backgroundColorAtom.reportRead();
+    return super.backgroundColor;
+  }
+
+  @override
+  set backgroundColor(Color value) {
+    _$backgroundColorAtom.reportWrite(value, super.backgroundColor, () {
+      super.backgroundColor = value;
+    });
+  }
+
+  late final _$levelCountAtom =
+      Atom(name: '_CountOneByOneViewModelBase.levelCount', context: context);
+
+  @override
+  int get levelCount {
+    _$levelCountAtom.reportRead();
+    return super.levelCount;
+  }
+
+  @override
+  set levelCount(int value) {
+    _$levelCountAtom.reportWrite(value, super.levelCount, () {
+      super.levelCount = value;
+    });
+  }
+
+  late final _$wrongAnswerSignalAsyncAction = AsyncAction(
+      '_CountOneByOneViewModelBase.wrongAnswerSignal',
+      context: context);
+
+  @override
+  Future<void> wrongAnswerSignal() {
+    return _$wrongAnswerSignalAsyncAction.run(() => super.wrongAnswerSignal());
+  }
+
+  late final _$_CountOneByOneViewModelBaseActionController =
+      ActionController(name: '_CountOneByOneViewModelBase', context: context);
+
+  @override
+  void userClick(String number) {
+    final _$actionInfo = _$_CountOneByOneViewModelBaseActionController
+        .startAction(name: '_CountOneByOneViewModelBase.userClick');
+    try {
+      return super.userClick(number);
+    } finally {
+      _$_CountOneByOneViewModelBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+backgroundColor: ${backgroundColor},
+levelCount: ${levelCount}
+    ''';
+  }
+}

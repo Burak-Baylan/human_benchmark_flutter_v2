46aa9537b6631fe7a88519e89d0ce689451b7dff
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-27 03:54:30 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:human_benchmark_flutter_v2/utils/injection_helper.dart';
-import 'ads/controller/ads_controller.dart';
 import 'helpers/colors.dart';
+import 'helpers/setup_app.dart';
 import 'pages/home/home_page.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  //MobileAds.instance.initialize();
-  setUpInjections();
+  SetupApp.setup();
   runApp(MyApp());
 }
 
@@ -23,7 +20,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    Get.put(AdsController());
     return ScreenUtilInit(
       designSize: Size(360, 844),
       builder: (BuildContext context, Widget? child) {

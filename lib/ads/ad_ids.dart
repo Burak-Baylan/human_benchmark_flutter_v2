5506d53d7e55de0dfadaf46c45f40ead9703353e
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-27 03:47:34 +0300

    add: ad ids

---
+++ lib/ads/ad_ids.dart
@@ -0,0 +1,82 @@
+class AddIds {
+  static const String appOpenId = 'ca-app-pub-9469178764435701/3271639989';
+
+  static const String reactionTimeBanner =
+      'ca-app-pub-9469178764435701/9148435589';
+  static const String reactionTimeInterstitial =
+      'ca-app-pub-9469178764435701/3403749347';
+
+  static const String numbersMemoryBanner =
+      'ca-app-pub-9469178764435701/6362490149';
+  static const String numbersMemoryInterstitial =
+      'ca-app-pub-9469178764435701/9597375184';
+
+  static const String sequenceMemoryBanner =
+      'ca-app-pub-9469178764435701/8284293515';
+  static const String sequenceMemoryInterstitial =
+      'ca-app-pub-9469178764435701/1718885161';
+
+  static const String fastFingersBanner =
+      'ca-app-pub-9469178764435701/8092721824';
+  static const String fastFingersInterstitial =
+      'ca-app-pub-9469178764435701/7452210531';
+
+  static const String vibrationBanner =
+      'ca-app-pub-9469178764435701/6139128862';
+  static const String vibrationInterstitial =
+      'ca-app-pub-9469178764435701/5979346763';
+
+  static const String findNumberBanner =
+      'ca-app-pub-9469178764435701/1143335947';
+  static const String finddNumberInterstitial =
+      'ca-app-pub-9469178764435701/5492810343';
+
+  static const String findColorBanner =
+      'ca-app-pub-9469178764435701/8382148820';
+  static const String findColorInterstitial =
+      'ca-app-pub-9469178764435701/4283121712';
+
+  static const String catchColorBanner =
+      'ca-app-pub-9469178764435701/2970040047';
+  static const String catchColorInterstitial =
+      'ca-app-pub-9469178764435701/5404631690';
+
+  static const String coloredTextBanner =
+      'ca-app-pub-9469178764435701/2778468353';
+  static const String coloredTextInterstitial =
+      'ca-app-pub-9469178764435701/6204090931';
+
+  static const String mathBanner = 'ca-app-pub-9469178764435701/7839223347';
+  static const String mathInterstitial =
+      'ca-app-pub-9469178764435701/9503658809';
+
+  static const String countOneByOnebanner =
+      'ca-app-pub-9469178764435701/4251332123';
+  static const String countOneByOneInterstitial =
+      'ca-app-pub-9469178764435701/8960733320';
+
+  static const String colorCellCountBanner =
+      'ca-app-pub-9469178764435701/7647651651';
+  static const String coloreCellCountBannerInterstitial =
+      'ca-app-pub-9469178764435701/6685923770';
+
+  static const String fallingBallsBanner =
+      'ca-app-pub-9469178764435701/4059760436';
+  static const String fallingBallsInterstitial =
+      'ca-app-pub-9469178764435701/2746678760';
+
+  static const String holdAndClickBanner =
+      'ca-app-pub-9469178764435701/9120515420';
+  static const String holdAndClickInterstitial =
+      'ca-app-pub-9469178764435701/2533273382';
+
+  static const String visualMemoryBanner =
+      'ca-app-pub-9469178764435701/6280946706';
+  static const String visualMemoryInterstitial =
+      'ca-app-pub-9469178764435701/7583238327';
+
+  static const String blindNumbersBanner =
+      'ca-app-pub-9469178764435701/3463211674';
+  static const String blindNumbersInterstitial =
+      'ca-app-pub-9469178764435701/2150130007';
+}

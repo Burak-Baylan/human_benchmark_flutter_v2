298515fdc6439ddea0e908ec1e207ac03973c623
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-28 02:42:59 +0300

    update: setup app

--- lib/helpers/setup_app.dart
+++ lib/helpers/setup_app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:human_benchmark_flutter_v2/ads/ad_manager.dart';
 import '../utils/injection_helper.dart';
 
 class SetupApp {
   static Future<void> setup() async {
     WidgetsFlutterBinding.ensureInitialized();
-    MobileAds.instance.initialize();
+    await MobileAds.instance.initialize();
+    await AdManager.loadAllInterstitialAds();
     setUpInjections();
   }
 }

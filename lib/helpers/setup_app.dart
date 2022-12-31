c4a2611fd9afe16ace729b189b92a4b1313f128f
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-31 23:11:53 +0300

    update: setup app

--- lib/helpers/setup_app.dart
+++ lib/helpers/setup_app.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:human_benchmark_flutter_v2/ads/ad_manager.dart';
+import 'package:human_benchmark_flutter_v2/core/hive/hive_helper.dart';
 import '../utils/injection_helper.dart';
 
 class SetupApp {
   static Future<void> setup() async {
+    await HiveHelper.instance.initHive();
     WidgetsFlutterBinding.ensureInitialized();
     await MobileAds.instance.initialize();
     await AdManager.loadAllInterstitialAds();

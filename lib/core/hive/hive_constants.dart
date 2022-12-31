e9461ea8db78ce09555fabb1e1d100df3e423749
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-31 23:10:53 +0300

    add: hive constants

---
+++ lib/core/hive/hive_constants.dart
@@ -0,0 +1,23 @@
+class HiveConstants {
+  static const String BOX_APP_PREFERENCES = 'box_app_preferences';
+
+  static const String BOX_REACTION_TIME_SCORES = 'box_reaction_time';
+  static const String BOX_NUMBERS_MEMORY_SCORES = 'box_numbers_memory';
+  static const String BOX_SEQUENCE_MEMORY_SCORES = 'box_sequence_memory';
+  static const String BOX_FAST_FINGERS_SCORES = 'box_fast_fingers';
+  static const String BOX_VIBRATION_SCORES = 'box_vibration';
+  static const String BOX_FIND_NUMBER_SCORES = 'box_find_number';
+  static const String BOX_FIND_COLOR_SCORES = 'box_find_color';
+  static const String BOX_CATCH_COLOR_SCORES = 'box_catch_color';
+  static const String BOX_COLORED_TEXT_SCORES = 'box_colored_text';
+  static const String BOX_MATH_SCORES = 'box_math';
+  static const String BOX_COUNT_ONE_BY_ONE_SCORES = 'box_count_one_by_one';
+  static const String BOX_COLORED_CELL_COUNT_SCORES = 'box_colored_cell_count';
+  static const String BOX_FALLING_BALLS_SCORES = 'box_falling_balls';
+  static const String BOX_HOLD_AND_CLICK_SCORES = 'box_hold_and_click';
+  static const String BOX_VISUAL_MEMORY_SCORES = 'box_visual_memory';
+  static const String BOX_AIM_TRAINER_SCORES = 'box_aim_trainer';
+  static const String BOX_BLIND_NUMBERS_SCORES = 'box_blind_numbers';
+
+  static const String KEY_FIRST_INIT = 'first_init_preference';
+}

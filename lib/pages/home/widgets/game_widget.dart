eaa15873e06f774cfb036789f7961b512f4ca5c8
Burak-Baylan <dsjkadnas@gmail.com>
2022-12-31 23:18:13 +0300

    update: game widget

--- lib/pages/home/widgets/game_widget.dart
+++ lib/pages/home/widgets/game_widget.dart
@@ -1,48 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import '../../../helpers/colors.dart';
-import '../../../widgets/button/custom_button_with_border.dart';
 import '../../../widgets/text/less_futured_text.dart';
+import '../../history_page/view/history_view.dart';
+import '../home_page.dart';
 
 class GamesWidget extends StatelessWidget {
-  GamesWidget({
-    Key? key,
-    required this.text,
-    required this.route,
-    required this.gameNumber,
-    this.onPressed,
-  }) : super(key: key);
+  GamesWidget({Key? key, required this.model}) : super(key: key);
 
-  String text;
-  Widget route;
-  String gameNumber;
-  Function? onPressed;
+  HomePageGameWidgetModel model;
 
   @override
   Widget build(BuildContext context) {
-    return CustomButtonWithBorder(
-      onPressed: () {
-        if (onPressed != null) onPressed!();
-        Get.to(route);
-      },
-      backgroundColor: Colors.white,
-      text: text,
-      textColor: MyColors.secondaryColor,
-      borderSideWidth: 3,
-      leading: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          LessText.lessFuturedText(
-            text: gameNumber,
-            fontWeight: FontWeight.w600,
-            color: MyColors.secondaryColor,
-            fontSize: 18,
-            textAlign: TextAlign.left,
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 12.w, vertical: 5.h),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.white,
+          side: BorderSide(color: MyColors.secondaryColor, width: 3),
+          elevation: 0,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
           ),
-        ],
+        ),
+        onPressed: () {
+          model.onPressed();
+          Get.to(model.route);
+        },
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 20.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 9,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            child: LessText.lessFuturedText(
+                              text: '${model.gameNumber}.',
+                              color: MyColors.secondaryColor,
+                              textAlign: TextAlign.center,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 17.sp,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 9,
+                          child: Container(
+                            child: LessText.lessFuturedText(
+                              text: model.name,
+                              color: MyColors.secondaryColor,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 17.sp,
+                              textAlign: TextAlign.start,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      margin: EdgeInsets.only(right: 5.w),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.history_rounded,
+                          color: MyColors.secondaryColor,
+                          size: 23.sp,
+                        ),
+                        onPressed: () => Get.to(
+                          HistoryView(
+                            historyBoxName: model.boxName,
+                            name: model.name,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    LessText.lessFuturedText(
+                      text: 'PR: ',
+                      color: MyColors.secondaryColor,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15.sp,
+                    ),
+                    LessText.lessFuturedText(
+                      text: '360ms',
+                      color: MyColors.secondaryColor,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 15.sp,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      isGameWidget: true,
-      size: Size(context.width / 1.2, context.height / 17),
     );
   }
 }

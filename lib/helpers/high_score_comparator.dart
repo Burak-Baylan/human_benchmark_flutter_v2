4fa57be6ae80651a46e28e35fb28da9a1d12039c
Burak-Baylan <dsjkadnas@gmail.com>
2023-01-01 19:21:03 +0300

    add: high score comparator

---
+++ lib/helpers/high_score_comparator.dart
@@ -0,0 +1,24 @@
+import '../core/hive/hive_manager.dart';
+
+class HightScoreComparator {
+  static Future<bool> compare({
+    required String boxName,
+    required int score,
+    bool compareAsLower = true,
+  }) async {
+    var previousHighScore =
+        await HiveManager.getData<int>(boxName, 'high_score');
+    previousHighScore ??= compareAsLower ? 999999999999999999 : 0;
+    if (compareAsLower) {
+      if (!(score < previousHighScore)) {
+        return false;
+      }
+    } else {
+      if (!(score > previousHighScore)) {
+        return false;
+      }
+    }
+    await HiveManager.putData<int>(boxName, score, 'high_score');
+    return true;
+  }
+}

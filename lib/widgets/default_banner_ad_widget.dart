ea12d22815e7e3d29fba13a1650a06e44f6a9d4a
Burak-Baylan <dsjkadnas@gmail.com>
2023-01-30 02:48:59 +0300

    update: isPremium (default banner ad widget)

--- lib/widgets/default_banner_ad_widget.dart
+++ lib/widgets/default_banner_ad_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:human_benchmark_flutter_v2/helpers/colorful_print.dart';
+import 'package:human_benchmark_flutter_v2/main.dart';
 
 import '../ads/ad_banner.dart';
 
@@ -16,6 +17,7 @@ class DefaultBannerAd extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (mainVm.isPremium) return Container();
     return SizedBox(
       width: AdSize.banner.width.toDouble(),
       height: AdSize.banner.height.toDouble(),

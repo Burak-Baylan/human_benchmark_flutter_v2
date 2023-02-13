ce2b711d2f52981d31b61a3d0924c8686dd4786b
Burak-Baylan <dsjkadnas@gmail.com>
2023-02-13 22:01:48 +0300

    add: tap wrapper widget

---
+++ lib/widgets/tap_wrapper.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TapWrapper extends StatefulWidget {
+  const TapWrapper({Key? key, required this.onTap, required this.child})
+      : super(key: key);
+
+  @override
+  State<TapWrapper> createState() => _TapWrapperState();
+
+  final VoidCallback onTap;
+  final Widget child;
+}
+
+class _TapWrapperState extends State<TapWrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: widget.onTap,
+      child: widget.child,
+    );
+  }
+}

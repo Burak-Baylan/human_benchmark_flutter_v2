fb6538d3356fff735cb9f6ce79f3a73da93b9d13
Burak-Baylan <dsjkadnas@gmail.com>
2023-02-13 21:58:38 +0300

    update: hive constants,

--- lib/core/hive/hive_constants.dart
+++ lib/core/hive/hive_constants.dart
@@ -1,6 +1,9 @@
 class HiveConstants {
   static const String BOX_APP_PREFERENCES = 'box_app_preferences';
 
+  static const String seenOnboard = 'seenOnboard';
+  static const String isPremium = 'isPremium';
+
   static const String BOX_REACTION_TIME_SCORES = 'box_reaction_time';
   static const String BOX_NUMBERS_MEMORY_SCORES = 'box_numbers_memory';
   static const String BOX_SEQUENCE_MEMORY_SCORES = 'box_sequence_memory';
@@ -19,26 +22,36 @@ class HiveConstants {
   static const String BOX_AIM_TRAINER_SCORES = 'box_aim_trainer';
   static const String BOX_BLIND_NUMBERS_SCORES = 'box_blind_numbers';
 
-  static const String BOX_REACTION_TIME_HIGH_SCORE = 'box_reaction_time_high_score';
-  static const String BOX_NUMBERS_MEMORY_HIGH_SCORE = 'box_numbers_memory_high_score';
-  static const String BOX_SEQUENCE_MEMORY_HIGH_SCORE = 'box_sequence_memory_high_score';
-  static const String BOX_FAST_FINGERS_HIGH_SCORE = 'box_fast_fingers_high_score';
+  static const String BOX_REACTION_TIME_HIGH_SCORE =
+      'box_reaction_time_high_score';
+  static const String BOX_NUMBERS_MEMORY_HIGH_SCORE =
+      'box_numbers_memory_high_score';
+  static const String BOX_SEQUENCE_MEMORY_HIGH_SCORE =
+      'box_sequence_memory_high_score';
+  static const String BOX_FAST_FINGERS_HIGH_SCORE =
+      'box_fast_fingers_high_score';
   static const String BOX_VIBRATION_HIGH_SCORE = 'box_vibration_high_score';
   static const String BOX_FIND_NUMBER_HIGH_SCORE = 'box_find_number_high_score';
   static const String BOX_FIND_COLOR_HIGH_SCORE = 'box_find_color_high_score';
   static const String BOX_CATCH_COLOR_HIGH_SCORE = 'box_catch_color_high_score';
-  static const String BOX_COLORED_TEXT_HIGH_SCORE = 'box_colored_text_high_score';
+  static const String BOX_COLORED_TEXT_HIGH_SCORE =
+      'box_colored_text_high_score';
   static const String BOX_MATH_HIGH_SCORE = 'box_math_high_score';
-  static const String BOX_COUNT_ONE_BY_ONE_HIGH_SCORE = 'box_count_one_by_one_high_score';
-  static const String BOX_COLORED_CELL_COUNT_HIGH_SCORE = 'box_colored_cell_count_high_score';
-  static const String BOX_FALLING_BALLS_HIGH_SCORE = 'box_falling_balls_high_score';
-  static const String BOX_HOLD_AND_CLICK_HIGH_SCORE = 'box_hold_and_click_high_score';
-  static const String BOX_VISUAL_MEMORY_HIGH_SCORE = 'box_visual_memory_high_score';
+  static const String BOX_COUNT_ONE_BY_ONE_HIGH_SCORE =
+      'box_count_one_by_one_high_score';
+  static const String BOX_COLORED_CELL_COUNT_HIGH_SCORE =
+      'box_colored_cell_count_high_score';
+  static const String BOX_FALLING_BALLS_HIGH_SCORE =
+      'box_falling_balls_high_score';
+  static const String BOX_HOLD_AND_CLICK_HIGH_SCORE =
+      'box_hold_and_click_high_score';
+  static const String BOX_VISUAL_MEMORY_HIGH_SCORE =
+      'box_visual_memory_high_score';
   static const String BOX_AIM_TRAINER_HIGH_SCORE = 'box_aim_trainer_high_score';
-  static const String BOX_BLIND_NUMBERS_HIGH_SCORE = 'box_blind_numbers_high_score';
+  static const String BOX_BLIND_NUMBERS_HIGH_SCORE =
+      'box_blind_numbers_high_score';
 
   static const String BOX_UNLOCKED_GAMES = 'box_unlocked_games';
-
 
   static const String KEY_FIRST_INIT = 'first_init_preference';
 }

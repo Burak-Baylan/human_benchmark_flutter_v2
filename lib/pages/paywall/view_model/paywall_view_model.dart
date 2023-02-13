40ab2c0204fdde4bc749d352d9ee278447820914
Burak-Baylan <dsjkadnas@gmail.com>
2023-02-13 22:01:18 +0300

    update: paywall view model

--- lib/pages/paywall/view_model/paywall_view_model.dart
+++ lib/pages/paywall/view_model/paywall_view_model.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: empty_catches
 
+import 'package:human_benchmark_flutter_v2/helpers/colorful_print.dart';
 import 'package:mobx/mobx.dart';
+
+import '../../../utils/purchase_helper.dart';
 
 part 'paywall_view_model.g.dart';
 
@@ -9,4 +12,16 @@ class PaywallViewModel = _PaywallViewModelBase with _$PaywallViewModel;
 abstract class _PaywallViewModelBase with Store {
   @observable
   bool isLoading = false;
+
+  Future<bool> purcaseForOnboardingPaywall() async {
+    print('A905281 aaaaa');
+    if (PurchaseHelper.shared.weekly == null) return false;
+    isLoading = true;
+    print('A905281 true');
+    var isPurchased = await PurchaseHelper.shared
+        .purchase(PurchaseHelper.shared.weekly!);
+    isLoading = false;
+    print('A905281 false | Is Purchased: $isPurchased');
+    return isPurchased;
+  }
 }

d9db69f2125fbaf8d322196fd0aeb279eb49110c
Burak-Baylan <dsjkadnas@gmail.com>
2023-02-21 19:13:32 +0300

    add: get storage helper | update: add new colors

---
+++ lib/helpers/open_link.dart
@@ -0,0 +1,10 @@
+import 'colorful_print.dart';
+
+Future<void> launchLink(String link, [bool openWithActivity = false]) async {
+  /* if (!await launchUrl(Uri.parse(link),
+      mode: openWithActivity
+          ? LaunchMode.externalApplication
+          : LaunchMode.platformDefault)) {
+    ColorfulPrint.red('Could not launch link: $link');
+  } */
+}
